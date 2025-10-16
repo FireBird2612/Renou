c8f7e8ac1dcdfc4861cd289b93ea375b8f7f6f56
Sanket <sanket.gunjal.wk@gmail.com>
2025-10-17 03:33:53 +0530

    added bit definitions for generic uart configuration

--- Inc/uart.h
+++ Inc/uart.h
@@ -3,6 +3,30 @@
 
 #include "stm32f4_uart.h"
 
+/*  UART BAUDRATES  */
+#define UBR12 1200U
+#define UBR24 2400U
+#define UBR96 9600U
+#define UBR192 19200U
+#define UBR384 38400U
+#define UBR576 57600U
+#define UBR1152 115200U
+
+/*  UART oprating modes */
+#define RX_ONLY 0U
+#define TX_ONLY 1U
+#define TX_RX 2U
+
+/*  UART configurable stop bits    */
+#define STP_1 0U
+#define STP_05 1U
+#define STP_2 2U
+#define STP_15 3U
+
+/*  UART configurable word length   */
+#define WORD_LEN_8 0U
+#define WORD_LEN_9 M_BIT_MSK
+
 /**
     @brief  This will initialze the uart instance by doing following procedures
             1. Enable the clock for USART instance and GPIO
@@ -11,6 +35,6 @@
     @param
     @return
 */
-void uart_init(void);
+void uart_init(uart_def *uInstance);
 
 #endif
