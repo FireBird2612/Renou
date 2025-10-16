9d566b4b097dc6ee80042e4073a11eac7ae4c4b1
Sanket <sanket.gunjal.wk@gmail.com>
2025-10-17 03:34:40 +0530

    uart global handle init

--- Src/main.c
+++ Src/main.c
@@ -1,6 +1,20 @@
 #include <stdint.h>
+#include "stm32f4_uart.h"
 #include "uart.h"
 
+/*  global variables    */
+uart_def usart2;
+
 int main(void) {
-    uart_init();
+
+  usart2.instance = USART2;
+  usart2.uart_init.BaudRate = UBR1152;
+  usart2.uart_init.Mode = TX_RX;
+  usart2.uart_init.StopBits = STP_1;
+  usart2.uart_init.WordLength = WORD_LEN_8;
+
+  uart_init(&usart2);
+
+  return 0;
+
 }
