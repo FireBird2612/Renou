5b718c62a6b0033f8999bb276f5e5263d5bc97e5
Sanket <sanket.gunjal.wk@gmail.com>
2025-10-19 00:15:48 +0530

    driver(uart): Currently the uart_init breaks the protability. - Sets the Tx/Rx/TxRx, GPIO pin such as PA2, PA3, PD.. for the usart2. - Baudrate calculation used the float for which the FPU has been enabled   in startup code. - Implemented a logic that extracts the gpio pin from the bit pos

--- Src/uart.c
+++ Src/uart.c
@@ -1,24 +1,86 @@
-#include "uart.h"
 #include "stdio.h"
+#include "stdint.h"
+
+/* Hardware specific includes */
+#include "stm32_clock.h"
+#include "stm32f4_gpio.h"
 #include "stm32f4_uart.h"
 #include <stdint.h>
 
+/* Generic layer */
+#include "uart.h"
+
 
 void uart_init(uart_def *uInstance) {
+  /* Check for passed parameters, could be made more robust */
   if (!uInstance) {
     uInstance->uart_state = UART_STATE_ERROR;
     return;
   }
+  /* Why do we need buffer checking at initialization?
   if (uInstance->uRxBuffer == NULL && uInstance->uTxBuffer == NULL) {
     uInstance->uart_state = UART_STATE_ERROR;
     return;
   }
-  
+  */
+
   /*  Set the GPIO Functionality here */
+  if (uInstance->instance == USART2) {
+    /* Enable Clock for GPIOA Port    */
+    gpio_init((uint32_t)(uInstance->gpio_port.GPIOxPortInstance));
+
+    /* Enable USART2 Clock as well  */
+    RCC_UARTX_CLK_EN(uInstance->instance);
+
+    /* From the GPIOxPinSetBits member extract the gpio pin number which ranges
+    from 0 to 15. The application is required to set the bit for the GPIO Pin
+    which need to be configured.
+    Ex: GPIO Pin PA2 = TX and PA3 = RX
+    | 15 | 14 | 13 | 12 | 11 | 10 | 9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+    | 0  | 0  | 0  | 0  | 0  | 0  | 0 | 0 | 0 | 0 | 0 | 0 | 1 | 1 | 0 | 0 |
+    */
+    uint8_t bit_pos, set_bits_found = 0;
+    /* Extract the pin number from the GPIOxPinSetBits */
+    for (bit_pos = 0; bit_pos < 16; bit_pos++) {
+      if ((uInstance->gpio_port.GPIOxPinSetBits >> bit_pos) & 1) {
+        set_bits_found++;
+        /* pass the bit position as gpio number */
+        GPIO_MODE_SET(uInstance->gpio_port.GPIOxPortInstance->GPIOx_MODER,
+                      bit_pos, uInstance->gpio_port.GPIOxMode);
+        GPIO_AF_SET(uInstance->gpio_port.GPIOxPortInstance, bit_pos, uInstance->gpio_port.GPIOxAF);
+      }
+      if (set_bits_found == uInstance->gpio_port.GPIOxPinCount) {
+        break;
+      }
+    }
+  }
 
   /* Enable UART, OVER8=0, 8 Data bit, */
-  uInstance->instance->USART_CR1 |=  UE_BIT_MSK;
+  uint32_t make_mask = 0;
 
+  /* set wordlength */
+  if (uInstance->uart_init.WordLength == WORD_LEN_9) {
+    make_mask |= WORD_LEN_9;
+  }
+  /* uart enable */
+  make_mask |= UE_BIT_MSK;  
+  uInstance->instance->USART_CR1 |= make_mask;
+
+  /* check for stop bits */
+  make_mask = 0;
+  if (uInstance->uart_init.StopBits == STP_05) {
+    make_mask |= STP_05;
+  } else if (uInstance->uart_init.StopBits == STP_2) {
+    make_mask |= STP_2;
+  } else if (uInstance->uart_init.StopBits == STP_15) {
+    make_mask |= STP_15;
+  }
+
+  if (make_mask) {
+    uInstance->instance->USART_CR2 |= make_mask;
+  }
+
+  /* Baudrate calculation */
   float fclk = 16000000.0, udiv;
   uint32_t mantissa;
   uint64_t frac;
@@ -26,7 +88,7 @@ void uart_init(uart_def *uInstance) {
 
   if (((uInstance->instance->USART_CR1 >> OVER8_BIT_POS) & 1) == 0) {
     // Oversampling by 16, default
-    udiv = (float)(fclk / 16 * uInstance->uart_init.BaudRate);
+    udiv = (float)(fclk / (16 * uInstance->uart_init.BaudRate));
     mantissa = (uint32_t)udiv;
     frac = (uint64_t)((udiv - mantissa) * 16);
     BRR = ((mantissa & 0xFFF) << 4) | (frac & 0xF);
@@ -38,7 +100,6 @@ void uart_init(uart_def *uInstance) {
     BRR = ((mantissa & 0xFFF) << 4) | (frac & 0x7);
   }
 
-  /* Set the calculated baudrate */
   uInstance->instance->USART_BRR = BRR;
 
   if (uInstance->uart_init.Mode == TX_RX) {
@@ -59,4 +120,15 @@ void uart_init(uart_def *uInstance) {
   uInstance->uart_state = UART_STATE_READY;
 
   return;
+}
+
+void uart_tx(uart_def *uInstance, uint8_t *data) {
+  while (*data) {
+    uInstance->instance->USART_DR = *data++;
+    while (((uInstance->instance->USART_SR >> 7) & 1) ==
+           0) /*  Keep polling for until Transmit Data Register becomes 1 */
+      ;
+  }
+  while (((uInstance->instance->USART_SR >> 6) & 1) == 0) /*  When TC = 1, End of transmission */
+    ;
 }
