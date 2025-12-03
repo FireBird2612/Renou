6be0c7f1fbac95864bced6ae81bb3db2a6cf8369
Sanket <sanket.gunjal.wk@gmail.com>
2025-12-03 23:58:07 +0530

    ulog: log level specific macro and change in the naming of ulog_level_t structure member fields

--- Inc/ulog.h
+++ Inc/ulog.h
@@ -5,12 +5,17 @@
 #include <stddef.h>
 
 typedef enum {
-  ULOG_INFO,
-  ULOG_WARN,
-  ULOG_ERROR
+  LOG_INFO,
+  LOG_WARN,
+  LOG_ERROR
 } ulog_level_t;
 
 void ulog_init(void);
 void ulog_log(ulog_level_t log_level, const char *fmt);
+
+/*  Macros for using the ulog  */
+#define ULOG_INFO(fmt) ulog_log(LOG_INFO, fmt)
+#define ULOG_WARN(fmt) ulog_log(LOG_WARN, fmt)
+#define ULOG_ERROR(fmt) ulog_log(LOG_ERROR, fmt)
 
 #endif
