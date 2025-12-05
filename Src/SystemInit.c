32b8da31bad0be902bd33c990f1b7ccd5b04952d
Sanket <sanket.gunjal.wk@gmail.com>
2025-12-05 21:44:29 +0530

    TIM2: Early TIM2 initialization
    - Call to initialize the TIM2
    - Comment the osc_select_hse() since there is no current support for
      selecting different frequencies for peripherals.
    - The current implementation of osc_select_hse() does not switch to HSE.

--- Src/SystemInit.c
+++ Src/SystemInit.c
@@ -1,8 +1,10 @@
-#include <stm32_clock.h>
+#include "stm32f4_timer.h"
+#include "stm32_clock.h"
 #include "ulog.h"
 
 void SystemInit(void) {
-  osc_select_hse();
+  //osc_select_hse(); For now we are using the HSI clock. 
+  timer_init_upcounter(TIM2);
   ulog_init();
   ULOG_INFO("ulog initialized with USART2 instance");
 }
